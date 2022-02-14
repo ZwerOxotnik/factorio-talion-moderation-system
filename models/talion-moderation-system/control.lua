01862e0b6c968e74048961b6a978950c37c772f6
ZwerOxotnik <zweroxotnik@gmail.com>
2022-02-14 18:20:39 +0300

    Update to 0.4.1

--- models/talion-moderation-system/control.lua
+++ models/talion-moderation-system/control.lua
@@ -11,15 +11,33 @@ work. If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>.
 local M = {}
 
 
+--#region Global data
+---@class mod_data
+---@type table<string, table>
+local mod_data
+
+---{{[player index] = tick}}
+---@class players_last_tick_rank_gift
+---@type table<number, number>
+local players_last_tick_rank_gift
+--#endregion
+
+
+--#region Constants
 local permissions_list = require("models/talion-moderation-system/permissions_list")
-local max_ranks = 10
+local MAX_RANK = 10
+--#endregion
 
 
 local function set_default_rank_by_event(event)
-	local player = game.get_player(event.player_index)
+	local player_index = event.player_index
+	local player = game.get_player(player_index)
 	if not (player and player.valid) then return end
 	if player.admin then return end
 
+	if player.connected then
+		players_last_tick_rank_gift[player_index] = event.tick
+	end
 	game.permissions.get_group(tostring(settings.global["tms-default-rank"].value)).add_player(player)
 end
 
@@ -31,7 +49,10 @@ M.on_nth_tick = {
 		local connected_players = game.connected_players
 		for i=1, #connected_players do
 			local player = connected_players[i]
-			if player.valid and player.online_time > 60 * 60 * 40 then
+			if player.valid and not player.admin and
+				players_last_tick_rank_gift[player.index] <= game.tick + 60 * 60 * 40
+			then
+				players_last_tick_rank_gift[player.index] = game.tick
 				local id = tonumber(player.permission_group.name)
 				if id then
 					local group2 = get_group(tostring(id + 1))
@@ -49,7 +70,27 @@ M.add_remote_interface = function()
 	remote.add_interface("talion-moderation-system", {})
 end
 
+local function link_data()
+	mod_data = global.free_market
+	players_last_tick_rank_gift = mod_data.players_last_tick_rank_gift
+end
+
+
 local function update_global_data()
+	global.free_market = global.free_market or {}
+	mod_data = global.free_market
+	mod_data.players_last_tick_rank_gift = mod_data.players_last_tick_rank_gift or {}
+
+	link_data()
+
+	for player_index, player in pairs(game.players) do
+		if player.connected then
+			players_last_tick_rank_gift[player_index] = game.tick
+		else
+			players_last_tick_rank_gift[player_index] = nil
+		end
+	end
+
 	local get_group = game.permissions.get_group
 	local create_group = game.permissions.create_group
 	for id, _ in pairs(permissions_list) do
@@ -59,7 +100,7 @@ local function update_global_data()
 	end
 
 	for id, _ in pairs(permissions_list) do
-		for i = tonumber(id), max_ranks do
+		for i = tonumber(id), MAX_RANK do
 			local group = get_group(tostring(id))
 			for _, action in pairs(permissions_list[tostring(i)]) do
 				group.set_allows_action(action, false)
@@ -78,6 +119,8 @@ local function update_global_data()
 	end
 end
 
+
+M.on_load = link_data
 M.on_init = update_global_data
 M.on_configuration_changed = function(event)
 	local mod_changes = event.mod_changes["talion-moderation-system"]
@@ -89,7 +132,20 @@ end
 
 M.events = {
 	[defines.events.on_player_created] = set_default_rank_by_event,
-	[defines.events.on_player_demoted] = set_default_rank_by_event
+	[defines.events.on_player_demoted] = set_default_rank_by_event,
+	[defines.events.on_player_joined_game] = function(event)
+		local player_index = event.player_index
+		local player = game.get_player(player_index)
+		if not (player and player.valid) then return end
+		if player.admin then return end
+		players_last_tick_rank_gift[player_index] = event.tick
+	end,
+	[defines.events.on_player_left_game] = function(event)
+		players_last_tick_rank_gift[event.player_index] = nil
+	end,
+	[defines.events.on_player_removed] = function(event)
+		players_last_tick_rank_gift[event.player_index] = nil
+	end
 }
 
 
@@ -134,7 +190,9 @@ M.commands = {
 			return
 		end
 
-		sender_rank = sender_rank - 1
+		if not sender.admin then
+			sender_rank = sender_rank - 1
+		end
 		target_rank = target_rank + 1
 		local get_group = game.permissions.get_group
 		if not (get_group(tostring(sender_rank)) and get_group(tostring(target_rank))) then
@@ -184,7 +242,10 @@ M.commands = {
 			return
 		end
 
-		sender_rank = sender_rank - 1
+
+		if not sender.admin then
+			sender_rank = sender_rank - 1
+		end
 		target_rank = target_rank - 1
 		local get_group = game.permissions.get_group
 		if not (get_group(tostring(sender_rank)) and get_group(tostring(target_rank))) then
